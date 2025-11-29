31af5c10d00c64bd08905799f72b25154278a12e
rizkyzhang <rizkyzhangdev@gmail.com>
2025-11-29 12:11:47 +0700

    chore: gitignore target at nested depth

--- week-1/tax-calculator/target/debug/deps/rust_by_practice-f8dc97be19c18b11.d
+++
@@ -1,5 +0,0 @@
-/home/coderz/Projects/personal/learn/rust-by-practice/week-1/tax-calculator/target/debug/deps/rust_by_practice-f8dc97be19c18b11.d: src/main.rs
-
-/home/coderz/Projects/personal/learn/rust-by-practice/week-1/tax-calculator/target/debug/deps/rust_by_practice-f8dc97be19c18b11: src/main.rs
-
-src/main.rs:
--- week-1/tax-calculator/target/debug/rust-by-practice.d
+++
@@ -1,1 +0,0 @@
-/home/coderz/Projects/personal/learn/rust-by-practice/week-1/tax-calculator/target/debug/rust-by-practice: /home/coderz/Projects/personal/learn/rust-by-practice/week-1/tax-calculator/src/main.rs
